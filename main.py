a68e1f999f9f7d87080262b76adf99223dcfdded
José Ferreira <josebazjunior09@gmail.com>
2024-08-07 16:00:28 -0300

    [feat]: add logging settings

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from repositories.user_repository import (
     create_user,
-    get_users,
-    get_user_by_id,
-    get_user_by_email,
-    update_user,
     delete_user,
+    get_user_by_email,
+    get_user_by_id,
+    get_users,
+    update_user,
 )
+from settings.logging_settings import logger
 
 if __name__ == "__main__":
-    created_user = create_user(
-        username="teste", password="teste", email="teste@teste.com", is_active=True
-    )
-    print(created_user)
+    logger.debug("teste debug")
+    logger.info("teste info")
+    logger.warning("teste warning")
+    logger.error("teste error")
+    # created_user = create_user(
+    #     username="teste", password="teste", email="teste@teste.com", is_active=True
+    # )
+    # print(created_user)
 
     # users = get_users(page=1, per_page=10)
     # print(users)
---
+++ settings/logging_settings.py
@@ -0,0 +1,39 @@
+import logging
+from settings.env_settings import settings
+
+
+class LoggingFormatter(logging.Formatter):
+    green = "\033[0;32m"
+    yellow = "\x1b[33;20m"
+    red = "\033[0;31m"
+    reset = "\x1b[0m"
+    format = (
+        "%(asctime)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    )
+
+    FORMATS = {
+        logging.DEBUG: green + format + reset,
+        logging.INFO: green + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+
+        return formatter.format(record)
+
+
+def init_logging():
+    logger = logging.getLogger()
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(settings.LOG_LEVEL)
+    stream_handler.setFormatter(LoggingFormatter())
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+logger = init_logging()
